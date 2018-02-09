fd5027aff9c28f5cfa92b40b256c1afee5fb59ab
Tran Sy Bao <transybao93@gmail.com>
2018-02-09 14:33:27 +0700

    Adding days, hours, minutes in preparation time

--- components/createDishForm/stepFive.js
+++ components/createDishForm/stepFive.js
@@ -55,6 +55,42 @@ export default class DishStepFive extends React.Component {
         }
     }
 
+    //- action when user fill cost
+    dayFill = ()=>{
+        //- get value and send data back
+        var current = this.refs.days.value;
+        if(current.length == 1)
+        {
+            var final = '';
+            final = current + ' day(s)';
+            this.refs.days.value = final;
+        }
+    }
+
+    //- action when user fill cost
+    hourFill = ()=>{
+        //- get value and send data back
+        var current = this.refs.hours.value;
+        if(current.length == 1)
+        {
+            var final = '';
+            final = current + ' hour(s)';
+            this.refs.hours.value = final;
+        }
+    }
+
+    //- action when user fill cost
+    minFill = ()=>{
+        //- get value and send data back
+        var current = this.refs.mins.value;
+        if(current.length == 1)
+        {
+            var final = '';
+            final = current + ' minute(s)';
+            this.refs.mins.value = final;
+        }
+    }
+
     render() {
         return (
             <div className="create_profile_step">
@@ -90,9 +126,9 @@ export default class DishStepFive extends React.Component {
                 <div className="container">
                     <h3>Preparation time</h3>
                     <div style={{ display: 'inline-flex' }}>
-                        <input ref="days" className="days" style={{ width: '25%', marginRight: '3%', textAlign: 'left' }} type="number" placeholder="days"/>
-                        <input ref="hours" className="hours" style={{ width: '50%' , marginRight: '3%', textAlign: 'center' }} type="number" placeholder="hours"/>
-                        <input ref="mins" className="mins" style={{ width: '25%', textAlign: 'right' }} type="number" placeholder="mins"/>
+                        <input ref="days" className="days" onChange={ this.dayFill } style={{ width: '25%', marginRight: '3%', textAlign: 'left' }} type="text" placeholder="days"/>
+                        <input ref="hours" className="hours" onChange={ this.hourFill } style={{ width: '40%' , marginRight: '3%', textAlign: 'center' }} type="text" placeholder="hours"/>
+                        <input ref="mins" className="mins" onChange={ this.minFill } style={{ width: '35%', textAlign: 'right' }} type="text" placeholder="mins"/>
                     </div>
                 </div>
                 <div className="container bottom-confirmation">
